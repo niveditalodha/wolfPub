a536cc41bc03d63459e9854a91a5e2beb8c206f9
KATHERINE CROSBY <41490938+kacrosby@users.noreply.github.com>
2022-04-05 01:41:46 -0400

    distributors tasks and operations
    Updated Distributor Tasks and Operations  and Created Orders Tasks and Operations

--- src/wolfPubDB/taskAndOperations/Distributors.java
+++ src/wolfPubDB/taskAndOperations/Distributors.java
@@ -31,6 +31,159 @@ public class Distributors{
         }
     }
 
+    public static ArrayList<Distributors> selectDistributorsByDistributorId(string distributorId) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where distributorId="+distributorId);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static ArrayList<Distributors> selectDistributorsByName(string name) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where name="+name);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<Distributors> selectDistributorsByType(string type) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where type="+type);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static ArrayList<Distributors> selectDistributorsByBalance(float balance) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where balance="+balance);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<Distributors> selectDistributorsByPhone(string phone) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where phone="+phone);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<Distributors> selectDistributorsByCity(string city) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where city="+city);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<Distributors> selectDistributorsByStreet(string street) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where street="+street);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public static ArrayList<Distributors> selectDistributorsByContactPerson(string contactPerson) throws SQLException{
+        try{
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ArrayList<Distributors> output = new ArrayList<>();
+            ResultSet res = stat.executeQuery("Select * from distributors where contactPerson="+contactPerson);
+            while (res.next()){
+                Distributors d = new Distributors(res.getString("distributorId"), res.getString("name"), res.getString("type"), res.getFloat("balance"), res.getString("phone"), res.getString("city"), res.getString("street"), res.getString("contactPerson"));
+                output.add(d);
+            }
+            conn.close();
+            return output;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
     public static Boolean addDistributor(String distributorId, String name, String type, Float balance, String phone, String city, String street, String contactPerson) throws SQLException{
         try {
             Connection conn = DBConnect.getConnection();
@@ -39,7 +192,7 @@ public class Distributors{
             stat.setString(1, distributorId);
             stat.setString(2, name);
             stat.setString(3, type);
-            stat.setFloat(4, balnace);
+            stat.setFloat(4, balance);
             stat.setString(5, phone);
             stat.setString(6, city);
             stat.setString(7, street);
@@ -61,7 +214,7 @@ public class Distributors{
             PreparedStatement stat = conn.prepareStatement(query);
             stat.setString(1, name);
             stat.setString(2, type);
-            stat.setFloat(3, balnace);
+            stat.setFloat(3, balance);
             stat.setString(4, phone);
             stat.setString(5, city);
             stat.setString(6, street);
---
+++ src/wolfPubDB/taskAndOperations/Orders.java
@@ -0,0 +1,108 @@
+package wolfPubDB.taskAndOperations;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import wolfPubDB.connect.*;
+import wolfPubDB.classes.Issue;
+
+public class Orders {
+
+    public static ArrayList<Orders> selectOrders() {
+        try {
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            ResultSet res = stat.executeQuery("Select * from orders");
+            ArrayList<Orders> output = new ArrayList<>();
+            while (res.next()) {
+                Orders p = new Orders(res.getString("orderId"), Date.valueOf(res.getDate("deadline")), res.getString("price"), Date.valueOf(res.getDate("orderDate")), res.getString("noOfCopies"), res.getString("shippingCost"), res.getString("publicationId"), res.getString("distributorId"));
+                output.add(p);
+            }
+            conn.close()
+            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ArrayList<Book> selectOrders(String orderId) throws SQLException{
+        try {   
+            Connection conn = DbConnect.getConnection();
+            ArrayList<Orders> output = new ArrayList<>();
+            Statement stat = conn.createStatement();
+            ResultSet res = stat.executeQuery("select * from orders where order="+orderId);
+            while (res.next()) {
+                Issue p = new Issue(res.getString("orderId"), Date.valueOf(res.getDate("deadline")), res.getString("price"), Date.valueOf(res.getDate("orderDate")), res.getString("noOfCopies"), res.getString("shippingCost"), res.getString("publicationId"), res.getString("distributorId"));
+                output.add(p);
+            }
+            conn.close();
+            return output;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static Boolean addOrders(String orderId, String name, String type, Float balance, String phone, String city, String street, String contactPerson) throws SQLException{
+        try {
+            Connection conn = DBConnect.getConnection();
+            String query = "insert into orders(orderId, name, type, balance, phone, city, stret, contactPerson) values (?,?,?,?,?,?,?,?)";
+            PreparedStatement stat = conn.prepareStatement(query);
+            stat.setString(1, orderId);
+            stat.setDate(2, deadline);
+            stat.setString(3, price);
+            stat.setDate(4, orderDate);
+            stat.setString(5, noOfCopies);
+            stat.setString(6, shippingCost);
+            stat.setString(7, publicationId);
+            stat.setString(8, distributorId);
+            stat.executeUpdate();
+            conn.close()
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+public static Boolean updateOrders(String publicationId, Date issueDate String type) {
+    try {
+        Connection conn = DBConnect.getConnection();
+        String query = "Update issue set deadline = ?, noOfCopies = ? where orderId =?";
+        PreparedStatement stat = conn.prepareStatement(query);
+        stat.setDate(1, deadline);
+        stat.setString(2, noOfCopies);
+        stat.setString(3, orderId);
+        stat.executeUpdate();
+        ResultSet res = stat.executeQuery("Select count(*) as total from issue where publicationId=" + publicationId);
+        int count = 0;
+        while (res.next()) {
+            count = res.getInt("total");
+        }
+        conn.commit();
+        if (count!=0){
+            conn.close()
+            return  true;
+        }
+        conn.close()
+        return false;
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return Boolean.valueOf(false);
+    }
+    public static Boolean deleteOrders(String orderId) {
+        try {
+            Connection conn = DBConnect.getConnection();
+            Statement stat = conn.createStatement();
+            stat.executeUpdate("DELETE FROM orders WHERE orderId= " + orderId);
+            conn.close()
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+}
