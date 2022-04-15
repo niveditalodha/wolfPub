5b6ec4dd0570d2def70f888b0252c3def13e65d5
Vishal Sharma <vishalsh94@yahoo.in>
2022-04-14 20:38:24 -0400

    Print formatting changed for reports
    Signed-off-by: Vishal Sharma <vishalsh94@yahoo.in>

--- src/wolfPubDB/classes/ReportClass.java
+++ src/wolfPubDB/classes/ReportClass.java
@@ -1,5 +1,6 @@
 package wolfPubDB.classes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,8 +11,10 @@ public class ReportClass {
     public List<String> resultKeys;
     public ResultClass results;
     List<Integer> maxColWidth;
+
     /**
      * Constructor for ReportClass
+     *
      * @param rKeys List of column headers that are displayed while printing
      *              the result table. The name and order of each key should
      *              exactly match what we expect SQL queries to return.
@@ -20,20 +23,60 @@ public class ReportClass {
     public ReportClass(List<String> rKeys, ResultClass rList) {
         resultKeys = rKeys;
         results = rList;
+        calculateColWidth();
+    }
+
+    private void calculateColWidth() {
+        maxColWidth = new ArrayList<>();
+        for (int i = 0; i < resultKeys.size(); i++) {
+            int max = resultKeys.get(i).length();
+            for (List<String> row : results.rows) {
+                if (max < row.get(i).length()) {
+                    max = row.get(i).length();
+                }
+            }
+            maxColWidth.add(max);
+        }
     }
 
     public static void printReport(ReportClass report) {
+        int index = 0;
+        for (int i = 0; i < report.resultKeys.size(); i++) {
+            printChar('-', '+', report.maxColWidth.get(i));
+        }
+        System.out.println();
         for (String key : report.resultKeys) {
-            System.out.print(key + "\t");
+            System.out.print(key);
+            printChar(' ', '|', report.maxColWidth.get(index++) - key.length());
+        }
+
+        System.out.println();
+        for (int i = 0; i < report.resultKeys.size(); i++) {
+            printChar('-', '+', report.maxColWidth.get(i));
         }
 
         for (List<String> row : report.results.rows) {
             System.out.println();
             for (int i = 0; i < report.resultKeys.size(); i++) {
-                System.out.print(row.get(i) + "\t");
+                System.out.print(row.get(i));
+                printChar(' ', '|', report.maxColWidth.get(i) - row.get(i).length());
             }
         }
         System.out.println();
+        for (int i = 0; i < report.resultKeys.size(); i++) {
+            printChar('-', '+', report.maxColWidth.get(i));
+        }
+        System.out.println();
     }
+
+    private static void printChar(char ch, char separator, int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(ch);
+        }
+        System.out.print(sb.toString() + ch + separator + ch);
+    }
+
 
 }
